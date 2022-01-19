48bba1f7aed3dd850c96efd57b081f17e40b76af
pantomassi <73038936+pantomassi@users.noreply.github.com>
2022-01-20 00:12:07 +0100

    updated settings

--- project_tm/settings.py
+++ project_tm/settings.py
@@ -23,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-^el!-fu#%an%g1^n-a^^7@j#)0vg726k=f5sx$m+&pcn_bj-6p'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ['ballsvolleyballs.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
